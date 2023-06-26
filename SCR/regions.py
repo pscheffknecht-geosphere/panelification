4813b39c53882a144885b7acc73c91f7c5386257
Phillip Scheffknecht <phillip.scheffknecht@geosphere.at>
2023-06-26 14:23:06 +0000

    add subdomain optimization to reach a better optimization of the 1 km grid spacing, which is assumed by the verification routines. Bugfix in the subdomain generation

--- SCR/regions.py
+++ SCR/regions.py
@@ -1,9 +1,11 @@
 # DEFINE REGIONS FOR PLOTTING
+import os
 import cartopy.crs as ccrs
 import pyproj
 import numpy as np
 import pyresample 
-import logging
+import pickle
+from math import sin, cos, sqrt, atan2, radians
 
 # ADD YOUR REGION HERE
 # example:
@@ -24,11 +26,13 @@ import logging
 # the Region class will use pyproj and numpy to generate a lon lat grid with
 # a grid spacing of 1 km, centered over central_longitude and central_latitude
 
+import logging
 logger = logging.getLogger(__name__)
 
 # set default thresholds for scoring and drawing, in case the user does not set them
 default_thresholds =  {'draw_avg' : 2., 'draw_max' : 25., 'score_avg' : 1., 'score_max' : 5. }
 
+logger.info("Yeah, it's working here")
 regions = {
     "Europe": {
         "central_longitude": 15.,
@@ -73,6 +77,7 @@ regions = {
                 "thresholds": {'draw_avg' : 2., 'draw_max' : 25., 'score_avg' : 1., 'score_max' : 5.},
             },
             "Finland_Small": {
+                # works for the small 200m domain
                 "central_longitude": 25.5, "central_latitude": 60.35, "x_size": 220., "y_size": 220.
             }
         }
@@ -83,6 +88,7 @@ regions = {
 
 class Region():
     def __init__(self, region_name="Europe", subdomains=["Default"]):
+        logger.info("But not here")
         self.name = region_name
         self.extent=regions[region_name]['extent']
         self.data_projection = ccrs.PlateCarree()
@@ -102,16 +108,16 @@ class Region():
     #         central_latitude = clat)
 
 
-    def __make_grid(self, subdomain):
+    def __make_grid(self, subdomain_data, k=1.):
         """ Use pyproj to generate a rectangular 1-km-grid on a steregraphic projection
         with given center and dimensions in km (grid points) in each direction"""
-        proj_string = "epsg:31287 +proj=stere +units=km +lon_0={:.3f} lat_0={:.3f}".format(
-                subdomain["central_longitude"], subdomain["central_latitude"])
+        proj_string = "epsg:31287 +proj=stere +units=km +lon_0={:.8f} lat_0={:.8f} k={:.8f}".format(
+                subdomain_data["central_longitude"], subdomain_data["central_latitude"], k)
         myproj=pyproj.Proj(proj_string)
-        NX = subdomain["x_size"]
-        NY = subdomain["y_size"]
-        X = np.arange(NX) - subdomain["x_size"] / 2.
-        Y = np.arange(NY) - subdomain["x_size"] / 2 
+        NX = subdomain_data["x_size"]
+        NY = subdomain_data["y_size"]
+        X = np.arange(NX) - subdomain_data["x_size"] / 2.
+        Y = np.arange(NY) - subdomain_data["y_size"] / 2.
         XX, YY = np.meshgrid(X, Y)
         lon,lat=myproj(XX, YY, inverse=True)
         return lon, lat
@@ -119,8 +125,8 @@ class Region():
     def __prep_subdomains(self, region_name, subdomain_name_list):
         self.subdomains = {}
         for subdomain_name in subdomain_name_list:
-        #for key, item in subdomain_name_list.items():
-            lon, lat = self.__make_grid(regions[region_name]["verification_subdomains"][subdomain_name])
+            k = self.__get_scale_factor(regions[region_name]["verification_subdomains"][subdomain_name])
+            lon, lat = self.__make_grid(regions[region_name]["verification_subdomains"][subdomain_name], k=k)
             if "thresholds" in regions[region_name]["verification_subdomains"][subdomain_name].keys():
                 thresholds = regions[region_name]["verification_subdomains"][subdomain_name]["thresholds"]
             else:
@@ -152,5 +158,98 @@ class Region():
             else:
                 logging.warning("""Your resampled data contains missing values! you can use --fix_nans to set them to 0., but this can change scores!""")
         return data_resampled, self.subdomains[subdomain_name]["lon"], self.subdomains[subdomain_name]["lat"]
+
+
+    def __make_subdomain_key(self, subdomain_data):
+        key_str = "{:.8f}_{:.8f}__{:.8f}_{:.8f}".format(
+                subdomain_data["central_longitude"],
+                subdomain_data["central_latitude"],
+                subdomain_data["x_size"],
+                subdomain_data["y_size"])
+        logger.info("Current subdomain key {:s}".format(key_str))
+        return key_str
+
+
+    def __calcualte_distance_on_globe(self, lon1, lat1, lon2, lat2):
+        # Approximate radius of earth in km
+        R = 6373.0
+        d = np.zeros(lon1.shape)
+        imax, jmax = lon1.shape
+        for ii in range(imax):
+            for jj in range(jmax):
+                dlon = radians(lon2[ii, jj]) - radians(lon1[ii, jj])
+                dlat = radians(lat2[ii, jj]) - radians(lat1[ii, jj])
+
+                a = sin(dlat / 2)**2 + cos(radians(lat1[ii, jj])) * cos(radians(lat2[ii, jj])) * sin(dlon / 2)**2
+                c = 2 * atan2(sqrt(a), sqrt(1 - a))
+                d[ii, jj] = R * c
+        return d
+
+    def __calculate_grid_error(self, arr, val, score="bias"):
+        """ return score for array and constant val"""
+        if score.lower() == "bias":
+            return np.mean(arr - val)
+        elif score.lower() == "mae":
+            return np.mean(np.abs(arr-val))
+        elif score.lower() == "rmse":
+            return np.sqrt(np.mean(np.square(np.abs(arr-val))))
         
+
+    def __calculate_optimal_k(self, subdomain_data):
+        nx = subdomain_data["x_size"]
+        ny = subdomain_data["y_size"]
+        d = 1.
+        k = 1.0
+        kstep = 0.01
+        ksteps = []
+        ds = [1e20]
+        score = "bias"
+        maxiter = 50
+        logger.info(" Optimizing k for selected projection parameters, maximum {:d} iterations:".format(maxiter))
+        for ii in range(maxiter):
+            if ii > 2:
+                if np.abs(ds[-1] - ds[-2]) < 0.0000001 or np.abs(ds[-1]) < 0.0000001:
+                    # either the last iteration had very little impact or the last score was great
+                    break
+                kstep = kstep * 0.66667 if np.abs(ds[-1]) < np.abs(ds[-2]) else -kstep
+                k += kstep
+            else:
+                k += 0.1 * kstep # do a small step to get directions first
+            lon2, lat2 = self.__make_grid(subdomain_data, k=k)
+            xend, yend = lon2.shape
+            distance21 = self.__calcualte_distance_on_globe(lon2[0:xend-2, :], lat2[0:xend-2, :], lon2[1:xend-1, :], lat2[1:xend-1, :])
+            distance22 = self.__calcualte_distance_on_globe(lon2[:, 0:yend-2], lat2[:, 0:yend-2], lon2[:, 1:yend-1], lat2[:, 1:yend-1])
+            dbias = 0.5 * (np.mean(distance21) + np.mean(distance22))
+            dist_err = 0.5 * (self.__calculate_grid_error(distance21, d, score=score) +
+                              self.__calculate_grid_error(distance22, d, score=score))
+            logger.info("  N = {:d}, k = {:.10f}, {:s} = {:.10f}".format(ii, k, score, dist_err))
+            ksteps.append(kstep)
+            ds.append(dist_err)
+        
+        logger.info(" Using k = {:.10f} for projection.".format(k))
+        return k
+
+
+    def __get_scale_factor(self, subdomain_data):
+        current_key = self.__make_subdomain_key(subdomain_data)
+        if os.path.isfile("subdomain_scale_factors.p"):
+            with open("subdomain_scale_factors.p", "rb") as f:
+                scale_factor_dict = pickle.load(f)
+        else:
+            logger.info("No stored scale factors found, the current one will be the first stored in the new file")
+            scale_factor_dict = {}
+        if current_key in scale_factor_dict.keys():
+            logger.info("Scale factor found: k = {:.8f}".format(scale_factor_dict[current_key]))
+            return scale_factor_dict[current_key]
+        else:
+            logger.info("No scale factor found for current subdomain, calculating new one...")
+            scale_factor = self.__calculate_optimal_k(subdomain_data)
+            logger.info("Updating scale factor dictionary with current key:")
+            logger.info(repr({current_key: scale_factor}))
+            scale_factor_dict[current_key] = scale_factor
+            logger.info("Updating scale factor pickle file.")
+            with open("subdomain_scale_factors.p", "wb") as f:
+                pickle.dump(scale_factor_dict, f)
+            return scale_factor
+
 
