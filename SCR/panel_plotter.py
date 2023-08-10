e027203bbd7aef60e1a7033ce99fd1b07947e540
Scheffknecht Phillip <phillip.scheffknecht@zamg.ac.at>
2023-08-10 07:39:40 +0000

    add relative mode to new fss plots

--- SCR/panel_plotter.py
+++ SCR/panel_plotter.py
@@ -94,7 +94,10 @@ def make_fss_rank_plot_axes(ax, args):
 def add_fss_plot_new(ax, sim, rank_vmax, jj, args):
     fss_rank_cols = ['white']*rank_vmax
     fss_rank_cols[0:5] = ['black', 'firebrick', 'limegreen', 'gold', 'silver', 'darkorange']
-    fss_cmap = mplcolors.ListedColormap(fss_rank_cols)
+    if args.fss_mode == 'relative':
+        fss_cmap = mpl.cm.get_cmap("RdYlBu")
+    elif args.fss_mode == 'ranks':
+        fss_cmap = mplcolors.ListedColormap(fss_rank_cols)
     # add rank diagram for FSS
     extent = (0, sim['fss_ranks'].shape[1], sim['fss_ranks'].shape[0], 0)
     ny, nx = sim['fss_ranks'].shape
@@ -103,7 +106,14 @@ def add_fss_plot_new(ax, sim, rank_vmax, jj, args):
         for xx in range(nx):
             xedge = -0.5 + np.array([xx+pad, xx+pad, xx+1-pad, xx+1-pad, xx+pad])
             yedge = -0.5 + np.array([yy+pad, yy+1-pad, yy+1-pad, yy+pad, yy+pad])
-            col = fss_rank_cols[sim['fss_ranks'][yy, xx]]
+            if args.fss_mode == 'ranks':
+                col = fss_rank_cols[sim['fss_ranks'][yy, xx]]
+            elif args.fss_mode == 'relative':
+                fss_rel = sim['fss_rel'][yy, xx] 
+                pos_in_cmap = 0.5 + fss_rel * 10. # map [-.05, .05] to [0, 1]
+                pos_in_cmap = 0. if pos_in_cmap < 0. else pos_in_cmap
+                pos_in_cmap = 1. if pos_in_cmap > 1. else pos_in_cmap
+                col = fss_cmap(pos_in_cmap)
             if sim['fss_ranks'][yy, xx] == 1 and yy < 9: #only for bad but not nan
                 if sim['fss_overestimated'].to_numpy()[yy,xx] > 0.:
                     xedget = -0.5 + np.array([xx+3*pad, xx+1-3*pad, xx+0.5, xx+3*pad])
