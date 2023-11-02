d4884f68406192c66a7fcb388ee464e0e0889822
Scheffknecht Phillip <phillip.scheffknecht@zamg.ac.at>
2023-11-02 10:04:05 +0000

    add intranet stuff

---
+++ SCR/forecast.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+. ~/.bashrc
+set -x
+if [ $# == 0 ]; then
+  rundate=$(date "+%Y%m%d%H" --date "now -13 hours")
+else
+  rundate=$1
+fi
+# 2021-06-28 replaced cosmo1 with cosmo1e and cosmod2 with icond2
+/ment_arch/pscheff/miniconda3-py3.8/envs/panelification_deode/bin/python main.py -n forecast -s ${rundate} -d 3 -l 12 15 --custom_experiments arome aromeesuite arome_1k arome_1k_sp claef-control claef-mean claef-median cosmo1e icond2 ecmwf icon-eu --draw > ../LOG/forecast_${rundate}.log
---
+++ SCR/prepare_for_web.py
@@ -0,0 +1,96 @@
+import os
+import glob
+import datetime as dt
+
+
+remote_base_path = "/modelle/prod/mgruppe/WEB/PRECIP_VERIF_PANELS/"
+
+
+def today():
+    t_ = dt.datetime.now() 
+    d_curr = dt.datetime(t_.year, t_.month, t_.day)
+    return(d_curr)
+
+
+def send_panels_to_mgruppe(d_curr=None):
+    d_curr = today() - dt.timedelta(days=1) if not d_curr else d_curr
+    flist = glob.glob('../PLOTS/*cast*'+d_curr.strftime("%Y%m%d")+"*png")
+    if flist == []:
+        print("No panels found for today, sending nothing.")
+    else:
+        for fil in flist:
+            os.system("scp "+fil+" mgruppe@vvhmod-dev:"+remote_base_path+".")
+
+
+def clean_old_panels(d_curr=today()):
+    for dd_diff in range(160, 167):  # do entire week, in case some days were skipped
+        newday = d_curr - dt.timedelta(days=dd_diff)
+        print("Checking for "+remote_base_path+"*"+newday.strftime("%Y%m%d")+"*.png")
+        old_files = glob.glob(remote_base_path+"*"+newday.strftime("%Y%m%d")+"*.png")
+        if len(old_files) > 0:
+            for fil in old_files:
+                delete_cmd = "ssh mgruppe@vvhmod-dev 'rm "+fil+"'"
+                print(delete_cmd)
+                # os.system(delete_cmd)
+
+def send_html_to_mgruppe():
+    os.system("scp verif_panels.html mgruppe@vvhmod-dev:/modelle/prod/mgruppe/WEB/HTML/.")
+    
+
+def get_file_lists(d_curr):
+    fclist = []
+    nclist = []
+    t_ = d_curr
+    while d_curr > t_ - dt.timedelta(days=160):
+        _fclist = glob.glob('../PLOTS/*forecast*'+d_curr.strftime("%Y%m%d")+"*png")
+        _nclist = glob.glob('../PLOTS/*nowcast*'+d_curr.strftime("%Y%m%d")+"*png")
+        for _fc in _fclist:
+            print("Found panel: {file}".format(
+                file = _fc))
+            fclist.append(_fc)
+        for _nc in _nclist:
+            print("Found panel: {file}".format(
+                file = _nc))
+            nclist.append(_nc)
+        d_curr += -dt.timedelta(days=1)
+    return fclist, nclist
+
+
+def make_file_list_html(flist):
+    fstr = ''
+    for fil in flist:
+        parts = fil.split("_")
+        if len(parts) == 8:
+            pretty_str = dt.datetime.strptime(parts[3], "%Y%m%d").strftime("%Y-%m-%d") + " " + \
+                parts[4].replace("UTC", " UTC") + " " + \
+                parts[5].replace("0", "").replace("h", " hour accumulated rain") + " " + \
+                parts[7].replace(".png", "")
+        elif len(parts) == 9:
+            pretty_str = dt.datetime.strptime(parts[3], "%Y%m%d").strftime("%Y-%m-%d") + " " + \
+                parts[4].replace("UTC", " UTC") + " " + \
+                parts[5].replace("0", "").replace("h", " hour accumulated rain") + " " + \
+                parts[7] + " "+parts[8].replace(".png", "")
+        fil = fil.replace("PLOTS", "PRECIP_VERIF_PANELS")    
+        fstr += "<a href=\""+fil+"\" target=\"_blank=\" rel=\"noopener noreferrer\">"+pretty_str+"</a><br>\n"
+    return fstr
+
+
+def complete_blank_html(d_work=None):
+    d_work = today() if not d_work else d_work
+    fclist, nclist = get_file_lists(d_work)
+    fcliststr = make_file_list_html(fclist)
+    ncliststr = make_file_list_html(nclist)
+    with open('blank.html', 'r') as htmlfile:
+        blank_html_str = htmlfile.read()
+        blank_html_str = blank_html_str.replace("FORECASTLINKLIST", fcliststr)
+        blank_html_str = blank_html_str.replace("NOWCASTLINKLIST", ncliststr)
+    with open('verif_panels.html', 'w') as outfile:
+        outfile.write(blank_html_str)
+
+
+# for manually updates to intranet:
+if __name__ == "__main__":
+    complete_blank_html()
+    send_panels_to_mgruppe()
+    send_html_to_mgruppe()
+    clean_old_panels()
