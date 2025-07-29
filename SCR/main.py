5fd45095802c65bab1b9beeef976dfc05d17f8b9
Phillip Scheffknecht <phillip.scheffknecht@geosphere.at>
2025-07-29 13:47:04 +0000

    add INCAplus analysis reading

--- SCR/main.py
+++ SCR/main.py
@@ -265,6 +265,8 @@ def main():
     # if args.parameter in ['precip', 'precip2', 'precip3', 'sunshine']:
     if args.precip_verif_dataset == "INCA":
         data_list = inca.read_INCA(data_list, start_date, end_date, args)
+    elif args.precip_verif_dataset == "INCAplus":
+        data_list = inca.read_incaPlus_netcdf_ana(data_list, start_date, end_date, args)
     elif args.precip_verif_dataset == "INCA_archive":
         data_list = inca.read_inca_netcdf_archive(data_list, start_date, end_date, args)
     elif args.precip_verif_dataset == "OPERA":
