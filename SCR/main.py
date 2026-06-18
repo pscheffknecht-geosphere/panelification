73c9176c4b9c2985c3528bc25e305ce37630bb5e
Phillip Scheffknecht <phillip.scheffknecht@geosphere.at>
2026-06-18 11:59:22 +0000

    remove country test mask

--- SCR/main.py
+++ SCR/main.py
@@ -390,7 +390,7 @@ def main():
                 sim["lon_resampled"] = _lon
                 sim["lat_resampled"] = _lat
                 sim["precip_data_resampled"] = _data
-            austria_mask.mask_data_list_to_austria(data_list)
+            # austria_mask.mask_data_list_to_austria(data_list)
             if args.fss_calc_mode and args.fss_method != "legacy":
                 logging.warning(f"fss_mode was set to {args.fss_calc_mode}, this is ignored unless fss_method is set to legacy!")
             Parallel(n_jobs=args.threads,backend='threading')(delayed(scoring.calc_scores)(sim, data_list[0], args) for ii, sim in enumerate(data_list))
