0dfd89a2bac050e2baf08670d66c0cc69bdfa856
Phillip Scheffknecht <phillip.scheffknecht@geosphere.at>
2026-09-08 12:52:20 +0000

    update geosphere inca path

--- SCR/inca_functions.py
+++ SCR/inca_functions.py
@@ -162,7 +162,9 @@ def read_INCAPlus_ANA(data_list, start_date, end_date, args):
     rr_tmp = None
     while tt <= end_date:
         dat_str = tt.strftime("%Y%m%d%H")
-        inca_file_path = f"/incaplus_arch1/iplus/out/INCAPlus_1h/inca/{tt.year}/{tt.month:02d}/{tt.day:02d}/INCAPlus_1h_RR_ANA_{dat_str}00.nc"
+        inca_file_path = f"/perm/anamod/iplus/output/incaplus_arch1/INCAPlus_1h/inca/{tt.year}/{tt.month:02d}/{tt.day:02d}/INCAPlus_1h_RR_ANA_{dat_str}00.nc"
+               # /perm/anamod/iplus/output/incaplus_arch1/INCAPlus_1h/inca/2026/08/27/INCAPlus_1h_RR_ANA_202608270100.nc
+        # inca_file_path = f"/incaplus_arch1/iplus/out/INCAPlus_1h/inca/{tt.year}/{tt.month:02d}/{tt.day:02d}/INCAPlus_1h_RR_ANA_{dat_str}00.nc"
         # inca_file_path = f"/incaplus_arch1/iplus/out/INCAPlus_1h/inca/{tt.year}/{tt.month:02d}/{tt.day:02d}/INCAPlus_1h_RR_15m_ANA_{dat_str}00.nc"
         if os.path.isfile(inca_file_path):
             logger.info(f"reading INCAPlus from {inca_file_path}")
