44571ee328a9ec203883f3fb54456f4750ef8883
Phillip Scheffknecht <phillip.scheffknecht@geosphere.at>
2026-09-14 13:35:27 +0000

    update experiment list

--- SCR/custom_experiments_geosphere.py
+++ SCR/custom_experiments_geosphere.py
@@ -291,10 +291,10 @@ experiment_configurations = {
                               "/ment_arch3/aladin/PRECIP_ARCH/%Y%m%d/claef_1k_esuite_%H+%LLLL.grb2"],
         "color"            : "royalblue"
     },
-    "arome_emu_1k": {
+    "arome-emu-1k": {
         "web_form_entry"   : True,
         "base_experiment"  : "claef1k-control",
-        "path_template"    : ["/ment_arch3/aladin/PRECIP_ARCH/%Y%m%d/arome_emu_1k_%H+%LLLL.grb"],
+        "path_template"    : ["/ment_arch3/aladin/PRECIP_ARCH/%Y%m%d/arome-emu-1k_%H+%LLLL.grb"],
         "color"            : "slateblue"
     },
     "claef1k-backup": {
@@ -344,6 +344,27 @@ experiment_configurations = {
         "init_interval"    : 24,
         "base_experiment"  : "claef1k-control",
         "path_template"    : ["/ment_arch3/aladin/PRECIP_ARCH/%Y%m%d/claef_1k_slhd_nofhqlm_%H+%LLLL:00.grb2"],
+        "color"            : "hotpink"
+    },
+    "claef-1k-slhd-tstep30": {
+        "web_form_entry"   : True,
+        "init_interval"    : 24,
+        "base_experiment"  : "claef1k-control",
+        "path_template"    : ["/ment_arch3/aladin/PRECIP_ARCH/%Y%m%d/claef_1k_slhd_tstep30_%H+%LLLL:00.grb2"],
+        "color"            : "crimson"
+    },
+    "claef-1k-slhd-kappa-75-A0": {
+        "web_form_entry"   : True,
+        "init_interval"    : 24,
+        "base_experiment"  : "claef1k-control",
+        "path_template"    : ["/ment_arch3/aladin/PRECIP_ARCH/%Y%m%d/claef_1k_slhd_kappa_75_A0_%H+%LLLL:00.grb2"],
+        "color"            : "mediumvioletred"
+    },
+    "claef-1k-slhd-kappa-90-A0": {
+        "web_form_entry"   : True,
+        "init_interval"    : 24,
+        "base_experiment"  : "claef1k-control",
+        "path_template"    : ["/ment_arch3/aladin/PRECIP_ARCH/%Y%m%d/claef_1k_slhd_kappa_90_A0_%H+%LLLL:00.grb2"],
         "color"            : "hotpink"
     },
     "claef1k-mean": {
